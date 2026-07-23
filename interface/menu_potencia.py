041213e19f17c7fecdbb9419667487eaefae186d
Bruno Vidal <brunogomesrosavidal@outlook.com>
2026-07-23 15:17:04 -0300

    fix: add constante SEPARADOR in menu_potencia

--- interface/menu_potencia.py
+++ interface/menu_potencia.py
@@ -1,16 +1,17 @@
 from util.converte_entrada import converte_entrada
 from util.sair_do_menu import sair_do_menu
+from util.constantes import SEPARADOR
 
 def menu_potencia():
     
     while True:
-            print("========================================")
+            print(SEPARADOR)
             print("            Potência Elétrica           ")
             print("")
             print("                P = E x I               ")
             print("                E = P / I               ")
             print("                I = P / E               ")
-            print("========================================")
+            print(SEPARADOR)
             print("Digite dois valores e deixe um em branco")
 
         #     potencia = converte_entrada(input("Potência: "))
@@ -18,6 +19,8 @@ def menu_potencia():
         #     corrente = converte_entrada(input("Corrente: "))
 
         #     print(calcular_potencia(potencia, tensao, corrente))
+            print(SEPARADOR)
+
             opcao = sair_do_menu()
            
             match opcao:
