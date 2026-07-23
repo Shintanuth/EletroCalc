d50e98250a9bad0ef6640291076ecdfb6bf5d437
Bruno Vidal <brunogomesrosavidal@outlook.com>
2026-07-23 20:06:30 -0300

    fix: add constante SEPARADOR in sair_do_menu

--- util/sair_do_menu.py
+++ util/sair_do_menu.py
@@ -1,9 +1,12 @@
+from util.constantes import SEPARADOR
+
 def sair_do_menu():
     while True:
-        print("============================")
+        print(SEPARADOR)
         print("[c] Continuar")
         print("[v] voltar ao menu principal")
         print("[s] sair")
+        print(SEPARADOR)
         opcao = input("Digite sua opção: ").strip().lower()
         
         if opcao in ("c","v","s"):
