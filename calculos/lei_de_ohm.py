1abb3562fd827344c7e22fd64c1a0498b9a8629d
Bruno Vidal <brunogomesrosavidal@outlook.com>
2026-07-23 10:05:19 -0300

    fix: adjustment in logic lei_de_ohm and corrige TypeError in converte_entrada

--- calculos/lei_de_ohm.py
+++ calculos/lei_de_ohm.py
@@ -8,23 +8,23 @@ def calcular_lei_de_ohm(tensao, corrente, resistencia):
     if tensao is None:    
         
         resultado = corrente * resistencia
-        return f'Tensão = {resultado}'
+        return resultado
     
-    elif corrente is None:
+    if corrente is None:
         
         if resistencia == 0:
             raise ValueError("A resitência não pode ser zero")
         
         resultado = tensao / resistencia
-        return f'Corrente = {resultado}'
+        return resultado
     
-    elif resistencia is None:
+    if resistencia is None:
     
         if corrente == 0:
             raise ValueError("A corrente não pode ser zero")
         
         resultado = tensao / corrente
-        return f'Resistência = {resultado}'
+        return resultado
     
     
     
