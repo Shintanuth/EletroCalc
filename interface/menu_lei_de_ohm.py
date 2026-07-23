6855b9507ecab1540cd03cc3d916a65c0e19e87a
Bruno Vidal <brunogomesrosavidal@outlook.com>
2026-07-23 15:15:29 -0300

    fix: add constante SEPARADOR in menu_lei_de_ohm

--- interface/menu_lei_de_ohm.py
+++ interface/menu_lei_de_ohm.py
@@ -2,24 +2,25 @@ from util.converte_entrada import converte_entrada
 from calculos.lei_de_ohm import calcular_lei_de_ohm
 from util.sair_do_menu import sair_do_menu
 from util.mostrar_resultado import mostrar_resultado
+from util.constantes import SEPARADOR
 
 def menu_lei_de_ohm():
     while True:
-            print("========================================")
+            print(SEPARADOR)
             print("               Lei de Ohm               ")
             print("")
             print("               I = E / R                ")
             print("               E = I x R                ")
             print("               R = E / I                ")
-            print("========================================")
+            print(SEPARADOR)
             print("Digite dois valores e deixe um em branco")
-            print("========================================")
+            print(SEPARADOR)
 
             try:
                 tensao = converte_entrada(input("Tensão: "))
                 corrente = converte_entrada(input("Corrente: "))
                 resistencia = converte_entrada(input("Resistência: "))
-                print("========================================")
+                print(SEPARADOR)
 
                 grandeza, resultado, unidade = (calcular_lei_de_ohm(tensao, corrente, resistencia))
                 print(mostrar_resultado(grandeza, resultado, unidade))
