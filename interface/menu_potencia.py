7918aae2a28b477e05e240050b882e3f49ffc198
Bruno Vidal <brunogomesrosavidal@outlook.com>
2026-07-23 20:30:01 -0300

    add logic calcular_potencia in menu_potencia

--- interface/menu_potencia.py
+++ interface/menu_potencia.py
@@ -1,5 +1,7 @@
 from util.converte_entrada import converte_entrada
 from util.sair_do_menu import sair_do_menu
+from calculos.potencia import calcular_potencia
+from util.mostrar_resultado import mostrar_resultado
 from util.constantes import SEPARADOR
 
 def menu_potencia():
@@ -14,12 +16,19 @@ def menu_potencia():
             print(SEPARADOR)
             print("Digite dois valores e deixe um em branco")
 
-        #     potencia = converte_entrada(input("Potência: "))
-        #     tensao = converte_entrada(input("Tensão: "))
-        #     corrente = converte_entrada(input("Corrente: "))
+            try:
+                potencia = converte_entrada(input("Potência: "))
+                tensao = converte_entrada(input("Tensão: "))
+                corrente = converte_entrada(input("Corrente: "))
+                print(SEPARADOR)
 
-        #     print(calcular_potencia(potencia, tensao, corrente))
-            print(SEPARADOR)
+                grandeza, resultado, unidade = (calcular_potencia(potencia, tensao, corrente))
+                print(mostrar_resultado(grandeza, resultado, unidade))
+                         
+            except ValueError as erro:
+                 print(erro)
+                 continue             
+             
 
             opcao = sair_do_menu()
            
