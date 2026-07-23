23ce791eb2cd0023718dfc62d40d67c84153eb21
Bruno Vidal <brunogomesrosavidal@outlook.com>
2026-07-23 15:14:00 -0300

    fix: add constante SEPARADOR in menu

--- interface/menu.py
+++ interface/menu.py
@@ -2,13 +2,14 @@ from interface.menu_lei_de_ohm import menu_lei_de_ohm
 from interface.menu_potencia import menu_potencia
 from interface.menu_reatancia_ind import menu_reatancia_indutiva
 from interface.menu_reatancia_cap import menu_reatancia_capacitiva
+from util.constantes import SEPARADOR
 
 def menu():
     
     while True:
-        print("========================================")
+        print(SEPARADOR)
         print("            ELETROCALC v0.1")
-        print("========================================")
+        print(SEPARADOR)
         print(" 1 - Lei de Ohm")
         print(" 2 - Potência Elétrica")
         print(" 3 - Reatância Indutiva" )
@@ -17,7 +18,7 @@ def menu():
         print(" 6 - Associação em Série")
         print(" 7 - Associação em Paralelo")
         print("[0] Sair")
-        print("========================================")
+        print(SEPARADOR)
 
         opcao = input("Escolha uma opção: ")
 
