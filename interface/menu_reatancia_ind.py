78710ed123fb95361ecbdda75266bef3992369cc
Bruno Vidal <brunogomesrosavidal@outlook.com>
2026-07-23 15:19:19 -0300

    fix: add constante SEPARADOR in menu_reatancia_ind

--- interface/menu_reatancia_ind.py
+++ interface/menu_reatancia_ind.py
@@ -1,15 +1,17 @@
 from util.sair_do_menu import sair_do_menu
+from util.constantes import SEPARADOR
 
 def menu_reatancia_indutiva():
     
     while True:
-            print("========================================")
+            print(SEPARADOR)
             print("          Reataância Indutiva ")
             print("           XL = 2 x π x ƒ x L ")
-            print("========================================")
+            print(SEPARADOR)
             print("Digite dois valores e deixe um em branco")
 
-            ...
+            
+            print(SEPARADOR)
 
             opcao = sair_do_menu()
             
