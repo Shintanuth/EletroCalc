63fd7d0d280290fe8b0aa2f1386ed8d60f3ce96b
Bruno Vidal <brunogomesrosavidal@outlook.com>
2026-07-24 10:33:24 -0300

    fix: adjustment in menu_reatancia_ind

--- interface/menu_reatancia_ind.py
+++ interface/menu_reatancia_ind.py
@@ -1,15 +1,13 @@
 from util.sair_do_menu import sair_do_menu
-from util.constantes import SEPARADOR
+from util.constantes import SEPARADOR, ESPACOS_DOIS, TITULO_MENU_REAT_IND
 
 def menu_reatancia_indutiva():
     
     while True:
             print(SEPARADOR)
-            print("          Reataância Indutiva ")
-            print("           XL = 2 x π x ƒ x L ")
+            print(ESPACOS_DOIS, TITULO_MENU_REAT_IND)
+            print(ESPACOS_DOIS, "XL = 2 x π x ƒ x L")
             print(SEPARADOR)
-            print("Digite dois valores e deixe um em branco")
-
             
             print(SEPARADOR)
 
