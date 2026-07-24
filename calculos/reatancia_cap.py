97fce3ddc78804a5fea829ac30468d47b84a2457
Bruno Vidal <brunogomesrosavidal@outlook.com>
2026-07-24 16:01:28 -0300

    fix: adjustment function calcular_reatancia_capacitiva

--- calculos/reatancia_cap.py
+++ calculos/reatancia_cap.py
@@ -8,11 +8,11 @@ def calcular_reatancia_capacitiva(frequencia, capacitancia):
     if frequencia <= 0:
         raise ValueError("A frequência não pode ser 0 e nem negativa")
 
+    if capacitancia is None:
+            raise ValueError("O valor da capacitância é obrigatório")
+
     if  capacitancia <= 0:
             raise ValueError("O valor da capacitância não pode ser 0 e nem negativo")
-
-    if capacitancia is None:
-        raise ValueError("O valor da capacitância é obrigatório")
 
     resultado = 1 / (2 * PI * frequencia * capacitancia)
     return "XC", resultado, OHM
--- interface/menu_reatancia_cap.py
+++ interface/menu_reatancia_cap.py
@@ -1,5 +1,8 @@
 from util.sair_do_menu import sair_do_menu
 from util.constantes import SEPARADOR, ESPACOS_DOIS, TITULO_MENU_REAT_CAP
+from util.converte_entrada import converte_entrada
+from util.mostrar_resultado import mostrar_resultado
+from calculos.reatancia_cap import calcular_reatancia_capacitiva
 
 def menu_reatancia_capacitiva():
     
@@ -8,7 +11,16 @@ def menu_reatancia_capacitiva():
             print(ESPACOS_DOIS, TITULO_MENU_REAT_CAP)
             print(ESPACOS_DOIS, "Xc = 1 / 2 π x ƒ x C")
             print(SEPARADOR)
-
+            try:
+                frequencia = converte_entrada(input("Frequência: "))
+                capacitancia = converte_entrada(input("Indutância: "))
+                print(SEPARADOR)
+            
+                grandeza, resultado, unidade = (calcular_reatancia_capacitiva(frequencia, capacitancia))
+                print(mostrar_resultado(grandeza, resultado, unidade))
+            
+            except ValueError as erro:
+                print(erro)    
 
             print(SEPARADOR)
 
