3fb812d233b2f596767f898ac5ec85577b41e753
Bruno Vidal <brunogomesrosavidal@outlook.com>
2026-07-24 10:02:21 -0300

    fix: adjustment in menu_lei_de_ohm

--- interface/menu_lei_de_ohm.py
+++ interface/menu_lei_de_ohm.py
@@ -2,18 +2,18 @@ from util.converte_entrada import converte_entrada
 from calculos.lei_de_ohm import calcular_lei_de_ohm
 from util.sair_do_menu import sair_do_menu
 from util.mostrar_resultado import mostrar_resultado
-from util.constantes import SEPARADOR
+from util.constantes import SEPARADOR, ESPACOS, TITULO_MENU_LEI_DE_OHM, MSG_DOIS_VALORES
 
 def menu_lei_de_ohm():
     while True:
             print(SEPARADOR)
-            print("               Lei de Ohm               ")
-            print("")
-            print("               I = E / R                ")
-            print("               E = I x R                ")
-            print("               R = E / I                ")
+            print(ESPACOS, TITULO_MENU_LEI_DE_OHM)
+            print()
+            print(ESPACOS, "I = E / R")
+            print(ESPACOS, "E = I x R")
+            print(ESPACOS, "R = E / I")
             print(SEPARADOR)
-            print("Digite dois valores e deixe um em branco")
+            print(MSG_DOIS_VALORES)
             print(SEPARADOR)
 
             try:
