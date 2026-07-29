58fc028cfe27818f8036121a01b0136deb2d6976
Bruno Vidal <brunogomesrosavidal@outlook.com>
2026-07-29 20:49:55 -0300

    refactor: ajustes em validações

---
+++ util/validacoes_eletricas.py
@@ -0,0 +1,22 @@
+from util.constantes import FREQUENCIA_PADRAO
+from util.validacoes import validar_maior_que_zero
+
+# ===========================
+# Validações Elétrica
+# ===========================
+
+def obter_frequencia(frequencia):
+
+    if frequencia is None:
+        frequencia = FREQUENCIA_PADRAO
+
+    return frequencia
+
+def validar_quantidade_none(valores, quantidade_none, mensagem):
+     if valores.count(None) != quantidade_none:
+            raise ValueError(mensagem)
+
+def validar_valores_positivos(valores):
+
+     for valor, mensagem in valores:
+          validar_maior_que_zero(valor, mensagem)
